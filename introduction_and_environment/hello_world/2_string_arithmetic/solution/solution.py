9c2c991b4388fb3a7242026de603a236af10541d
FiodorG <gre_dorelus@yahoo.com>
2020-07-31 10:14:51 -0400

    Fixed push test

--- introduction_and_environment/hello_world/2_string_arithmetic/solution/solution.py
+++ introduction_and_environment/hello_world/2_string_arithmetic/solution/solution.py
@@ -1,7 +1,14 @@
 # Code your solution here
+<<<<<<< HEAD
 # string1 = "Hey"
 # string2 = "you!"
 # concat = string1 + string2
 # print(concat)
 # print(string1 * 3)
 STR_ARITHMETIC = 2
+=======
+# mult = ""*"
+# add = "+"
+string_arithmethic = str("+")
+string_arithmethic == 2
+>>>>>>> Fixed push test
--- introduction_and_environment/unix_and_bash/2_bash_commands/solution/solution.py
+++ introduction_and_environment/unix_and_bash/2_bash_commands/solution/solution.py
@@ -3,7 +3,14 @@
 ONE = 'grep elephant book.txt'
 TWO = 'mv notes.txt newdir'
 THREE = 'grep -r string testdir'
+<<<<<<< HEAD
 FOUR = 'rmdir'
 FIVE = '/'
 SIX = 'rm -r' # very dangerous 
 # SEVEN = ''
+=======
+FOUR = ''
+FIVE = ''
+SIX = ''
+SEVEN = ''
+>>>>>>> Fixed push test
