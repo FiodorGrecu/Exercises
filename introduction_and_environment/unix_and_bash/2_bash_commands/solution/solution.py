e7174522d102ff147545340e4bc030baa46af620
FiodorG <gre_dorelus@yahoo.com>
2020-07-31 11:14:12 -0400

    confusing prob description

--- introduction_and_environment/unix_and_bash/2_bash_commands/solution/solution.py
+++ introduction_and_environment/unix_and_bash/2_bash_commands/solution/solution.py
@@ -4,13 +4,19 @@ ONE = 'grep elephant book.txt'
 TWO = 'mv notes.txt newdir'
 THREE = 'grep -r string testdir'
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> confusing prob description
 FOUR = 'rmdir'
 FIVE = '/'
 SIX = 'rm -r' # very dangerous 
 # SEVEN = ''
+<<<<<<< HEAD
 =======
 FOUR = ''
 FIVE = ''
 SIX = ''
 SEVEN = ''
 >>>>>>> Fixed push test
+=======
+>>>>>>> confusing prob description
