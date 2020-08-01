90e93663c4e9c14b27f9dea9bfced5a43cf66c9a
FiodorG <gre_dorelus@yahoo.com>
2020-07-31 23:56:28 -0400

    Comented are the answer but only works if STR_ARITHMETIC = 2 given

--- introduction_and_environment/hello_world/2_string_arithmetic/solution/solution.py
+++ introduction_and_environment/hello_world/2_string_arithmetic/solution/solution.py
@@ -1,5 +1,7 @@
 # Code your solution here
-# mult = ""*"
-# add = "+"
-string_arithmethic = str("+")
-string_arithmethic == 2
+# string1 = "Hey"
+# string2 = "you!"
+# concat = string1 + string2
+# print(concat)
+# print(string1 * 3)
+STR_ARITHMETIC = 2
