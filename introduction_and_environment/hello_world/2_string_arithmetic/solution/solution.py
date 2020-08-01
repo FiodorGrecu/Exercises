7a9896ab6e5ff3c805fe349ec7ab63c9b8d06b13
FiodorG <gre_dorelus@yahoo.com>
2020-07-31 23:56:28 -0400

    Comented are the answer but only works if STR_ARITHMETIC = 2 given

--- introduction_and_environment/hello_world/2_string_arithmetic/solution/solution.py
+++ introduction_and_environment/hello_world/2_string_arithmetic/solution/solution.py
@@ -1,10 +1,14 @@
 # Code your solution here
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Comented are the answer but only works if STR_ARITHMETIC = 2 given
 # string1 = "Hey"
 # string2 = "you!"
 # concat = string1 + string2
 # print(concat)
 # print(string1 * 3)
+<<<<<<< HEAD
 STR_ARITHMETIC = 2
 =======
 # mult = ""*"
@@ -12,3 +16,6 @@ STR_ARITHMETIC = 2
 string_arithmethic = str("+")
 string_arithmethic == 2
 >>>>>>> Fixed push test
+=======
+STR_ARITHMETIC = 2
+>>>>>>> Comented are the answer but only works if STR_ARITHMETIC = 2 given
