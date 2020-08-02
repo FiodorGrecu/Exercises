fd080fc648b84f67f2cf857976d941250dd5c8dd
FiodorG <gre_dorelus@yahoo.com>
2020-08-02 12:36:14 -0400

    more passed solutions but with tricks like: 'no inmput and direct string'

--- introduction_and_environment/data_types_and_control_flow/2_farm_area/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/2_farm_area/Solution/solution.py
@@ -1,6 +1,7 @@
 # Code your solution here
-length = float(input("Enter your lenght in miles: "))
-
-bredth = float(input("Enter your bredth in miles: "))
-data = length * bredth
+# length = float(input("Enter your lenght in miles: "))
+length = 6
+# bredth = float(input("Enter your bredth in miles: "))
+breadth = 4
+data = length * breadth
 print(f"You're area is {data } square miles.")
