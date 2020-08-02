2da3cd25394232303f5096e7de05de88eac64985
FiodorG <gre_dorelus@yahoo.com>
2020-08-02 10:49:33 -0400

    solution 2_vowels_consonants works if no imput is given :)

--- introduction_and_environment/data_types_and_control_flow/2_vowel_consonant/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/2_vowel_consonant/Solution/solution.py
@@ -1,1 +1,9 @@
 # Code your solution here
+char = "a"
+# char = input("Enter a character: ")
+vowels = ["a","e","i","o","u"]
+
+if char in vowels:
+    print("vowel") 
+else:
+    print("not vowel") 
