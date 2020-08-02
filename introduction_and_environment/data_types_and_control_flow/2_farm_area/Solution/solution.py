1080b8d51ee106f61b544deef9c2a24c0bc44f47
FiodorG <gre_dorelus@yahoo.com>
2020-08-01 22:52:20 -0400

    Farm area is correct for sure doesnt pass has Input in it

--- introduction_and_environment/data_types_and_control_flow/2_farm_area/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/2_farm_area/Solution/solution.py
@@ -1,1 +1,6 @@
 # Code your solution here
+length = float(input("Enter your lenght in miles: "))
+
+bredth = float(input("Enter your bredth in miles: "))
+data = length * bredth
+print(f"You're area is {data } square miles.")
