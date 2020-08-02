5f482486b10b1b58816e3f8d9878b7807af19b22
FiodorG <gre_dorelus@yahoo.com>
2020-08-02 12:36:14 -0400

    more passed solutions but with tricks like: 'no inmput and direct string'

--- introduction_and_environment/data_types_and_control_flow/2_except_letter/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/2_except_letter/Solution/solution.py
@@ -1,1 +1,12 @@
 # Code your solution here
+string = "BYTE ACADEMY"
+# string = string.lower()
+# string = string.replace(' ','')
+data = []
+vowels = ["A","E","I","O","U"]
+for i in range(0,len(string),1):
+    if string[i] in vowels:
+        pass
+    elif string[i] not in vowels:
+        data.append(string[i])
+print(data)
