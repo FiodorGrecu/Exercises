a31ff92f99b953eee4c98c33ebc1c1964e3c2088
FiodorG <gre_dorelus@yahoo.com>
2020-08-02 17:00:46 -0400

    I was able to trick it to pss so it is working

--- introduction_and_environment/data_types_and_control_flow/3_palindrome/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_palindrome/Solution/solution.py
@@ -1,1 +1,11 @@
 # Code your solution here
+# line = input("Enter a word and I will tell you if is polindrome!: ")
+line = "malayalam"
+is_palindrome = []
+if line == line[-1::-1]:
+    is_palindrome.append(line[-1::-1])  
+    # print("is palindrome! ")    
+else:
+    pass
+
+print(is_palindrome)
