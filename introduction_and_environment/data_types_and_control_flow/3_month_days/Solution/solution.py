69e3fd074be4242c19478f46dd881543001f6b5e
FiodorG <gre_dorelus@yahoo.com>
2020-08-02 16:13:17 -0400

    3_month days doesn't pass

--- introduction_and_environment/data_types_and_control_flow/3_month_days/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_month_days/Solution/solution.py
@@ -1,1 +1,24 @@
 # Code your solution here
+month = input("Enter a month and I'll tel you how many days it has: ")
+
+# month = {"January":"31"} 
+month_dict = {"January":"31",
+              "February":"28,29{#leap year}",
+              "March":"31",
+               "April":"30",
+               "May":"31",
+               "June":"30",
+               "July":"31",
+               "August":"31",
+               "September":"30",
+               "October":"31",
+               "November":"30",
+               "December":"31"
+
+}
+def data(month):
+    if month in month_dict.keys():
+        return month_dict[month]
+    else:
+        return None
+print(data(month))
--- introduction_and_environment/data_types_and_control_flow/3_string_case/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_string_case/Solution/solution.py
@@ -1,1 +1,5 @@
 # Code your solution here
+string_val = "Beautiful World"# input("Enter a word and I will turn it into upper case: ")
+upper_data = string_val.upper()
+lower_data = string_val.lower()
+print(upper_data, lower_data)
