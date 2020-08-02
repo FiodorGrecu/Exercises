10e4bfa1358313640b2f665497cae3cd52a27d84
FiodorG <gre_dorelus@yahoo.com>
2020-08-02 12:36:14 -0400

    more passed solutions but with tricks like: 'no inmput and direct string'

--- introduction_and_environment/data_types_and_control_flow/2_farm_area/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/2_farm_area/Solution/solution.py
@@ -1,14 +1,20 @@
 # Code your solution here
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> more passed solutions but with tricks like: 'no inmput and direct string'
 # length = float(input("Enter your lenght in miles: "))
 length = 6
 # bredth = float(input("Enter your bredth in miles: "))
 breadth = 4
 data = length * breadth
+<<<<<<< HEAD
 =======
 length = float(input("Enter your lenght in miles: "))
 
 bredth = float(input("Enter your bredth in miles: "))
 data = length * bredth
 >>>>>>> Farm area is correct for sure doesnt pass has Input in it
+=======
+>>>>>>> more passed solutions but with tricks like: 'no inmput and direct string'
 print(f"You're area is {data } square miles.")
