53afbdf8f67d7461ee6dd72e567538b1e30880fc
FiodorG <gre_dorelus@yahoo.com>
2020-08-02 16:13:17 -0400

    3_month days doesn't pass

--- introduction_and_environment/data_types_and_control_flow/3_month_days/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_month_days/Solution/solution.py
@@ -1,5 +1,9 @@
 # Code your solution here
+<<<<<<< HEAD
 month = (input("Enter a month and I'll tel you how many days it has: "))
+=======
+month = input("Enter a month and I'll tel you how many days it has: ")
+>>>>>>> 3_month days doesn't pass
 
 # month = {"January":"31"} 
 month_dict = {"January":"31",
@@ -16,6 +20,7 @@ month_dict = {"January":"31",
                "December":"31"
 
 }
+<<<<<<< HEAD
 
 def data(month,month_dict):
     if month in month_dict.keys(): 
@@ -23,3 +28,11 @@ def data(month,month_dict):
     else:
         return None
 print(data(month,month_dict))
+=======
+def data(month):
+    if month in month_dict.keys():
+        return month_dict[month]
+    else:
+        return None
+print(data(month))
+>>>>>>> 3_month days doesn't pass
