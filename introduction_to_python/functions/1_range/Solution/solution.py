1595a6bdb2d53b932c38c3e05838befb664af57c
FiodorG <gre_dorelus@yahoo.com>
2020-08-03 15:18:51 -0400

    solution to range and return hello

--- introduction_to_python/functions/1_range/Solution/solution.py
+++ introduction_to_python/functions/1_range/Solution/solution.py
@@ -1,3 +1,8 @@
 # Code your solution here
+# range_100 = input("Enter a number: ")
+
 def range_100(number):
-    return
+    if number <= 100:
+        return "GREATNESS"
+    else:
+        return "OOPS"   
