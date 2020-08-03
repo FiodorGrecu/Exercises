c061c52401f4e77e06145d0661bd89ee502ea8d5
FiodorG <gre_dorelus@yahoo.com>
2020-08-03 11:33:54 -0400

    solution to  month_day and Shape check

--- introduction_and_environment/data_types_and_control_flow/3_shape_check/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_shape_check/Solution/solution.py
@@ -1,1 +1,19 @@
 # Code your solution here
+figure_list = {
+                "3":"Triangle",
+                "4":"Quadrilateral",
+                "5":"Pentagon",
+                "6":"Hexagon",
+                "7":"Heptagon",
+                "8":"Octagon", 
+                "9":"Nonagon"
+}
+data = []
+# figure = input("figure: ")
+figure = "5"
+if figure in figure_list:
+    data.append(figure_list[figure])
+    print (figure_list[figure])
+    
+else:        
+    print(figure) 
