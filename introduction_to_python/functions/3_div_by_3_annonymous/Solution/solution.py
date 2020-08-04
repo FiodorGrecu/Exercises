0b51aa138b4041a53407f8b33b04d1a8201acaee
FiodorG <gre_dorelus@yahoo.com>
2020-08-04 16:50:59 -0400

    solution passed

--- introduction_to_python/functions/3_div_by_3_annonymous/Solution/solution.py
+++ introduction_to_python/functions/3_div_by_3_annonymous/Solution/solution.py
@@ -1,9 +1,12 @@
 # Code your solution here
 def div_by_3(*args):
 
-    return lambda args : args % 3 
-print(div_by_3())
+    return list(filter(lambda x: x % 3 == 0 ,args))
+# print(div_by_3(2, 3, 5, 6, 9))
 
+#     return lambda args :  
+# print(div_by_3())
+   
 
 # list1 = [1,2,3,4,5,6]
 
