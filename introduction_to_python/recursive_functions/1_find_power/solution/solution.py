b43d35b6f85364e803acbd639d0390c3e37d481b
FiodorG <gre_dorelus@yahoo.com>
2020-08-04 23:11:37 -0400

    solution to power

--- introduction_to_python/recursive_functions/1_find_power/solution/solution.py
+++ introduction_to_python/recursive_functions/1_find_power/solution/solution.py
@@ -1,3 +1,9 @@
 # Write your solution here
 def power(a, b):
-    return
+    if b == 0:
+        return 1
+    elif a == 0:
+        return 0
+    else:
+        return a * power(a, b-1)
+print(power(2, 3))
