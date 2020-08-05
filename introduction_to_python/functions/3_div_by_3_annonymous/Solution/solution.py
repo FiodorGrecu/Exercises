07ae6baa2d557c8044398ea87b0c7536c1ef7af4
FiodorG <gre_dorelus@yahoo.com>
2020-08-04 21:14:41 -0400

    division by 3 anonymus solution

--- introduction_to_python/functions/3_div_by_3_annonymous/Solution/solution.py
+++ introduction_to_python/functions/3_div_by_3_annonymous/Solution/solution.py
@@ -3,15 +3,3 @@ def div_by_3(*args):
 
     return list(filter(lambda x: x % 3 == 0 ,args))
 # print(div_by_3(2, 3, 5, 6, 9))
-
-#     return lambda args :  
-# print(div_by_3())
-   
-
-# list1 = [1,2,3,4,5,6]
-
-# for i in list1:
-#     i = lambda i : i % 3
-
-#      if i % 3 == 0:
-#     print(i)
