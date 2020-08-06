b1beaf1303c1538af3b645fc2121a119b42ca9d4
FiodorG <gre_dorelus@yahoo.com>
2020-08-06 10:43:20 -0400

    implement pow solution exe 1_implement_pow  of classes

--- introduction_to_python/classes/1_implement_pow/solution/solution.py
+++ introduction_to_python/classes/1_implement_pow/solution/solution.py
@@ -1,3 +1,25 @@
 class py_solution:
-   def pow(self, x, n):
-        ...
+
+   # def __init__(self, fnc):
+   #    self.fnc = fnc
+   #    self.result = []
+   
+
+   # def __call__(self):
+   #    return_result = self.fnc()
+   #    self.result.append(return_result)
+   #    return return_result
+   
+   # def result(self):
+   #    return self.result
+      
+
+   def pow(self,x, n):
+      if n == 0:
+         return 1
+      elif x == 0:
+         return 0
+      else:
+         return x * pow(x, n-1)
+print(pow(2,-3))  
+# print(pow.result(3,4))
