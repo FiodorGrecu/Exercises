9f71b1edcecf2e5545a4be50c6416ac6b4c9da2b
FiodorG <gre_dorelus@yahoo.com>
2020-08-05 20:46:24 -0400

    Solution convert upper and together wit teamate  did multiple inheritance

--- introduction_to_python/classes/3_multiple_inheritance/solution/solution.py
+++ introduction_to_python/classes/3_multiple_inheritance/solution/solution.py
@@ -1,21 +1,36 @@
 class Clock(object): 
-    def __init__(self, hrs=0, mins=0, secs=0): 
-        pass
+    def __init__(self, hrs=0, mins=0, secs=0):
+        self.hrs = hrs
+        self.mins = mins
+        self.secs = secs
+
+    # print(Clock())
 
     def __str__(self):
-        return
+        return "{0:02d}:{1:02d}:{2:02d}".format(self.hrs, self.mins, self.secs)
+# print(Clock(1,20,30))
   
 class Calendar(object): 
     def __init__(self, day=1, month=1, year=2020):
-        pass
+        self.day = day
+        self.month = month
+        self.year = year    
 
     def __str__(self):
-        return
-        
+
+        return "{0:02d}/{1:02d}/{2:4d}".format(self.day, self.month, self.year)
+print(Calendar(1,2,2020))   
   
 class CalendarClock(Clock, Calendar): 
     def __init__(self, day, month, year, hrs, mins, secs):
-        pass
+        self.day = day
+        self.month = month
+        self.year = year
+        self.hrs = hrs
+        self.mins = mins
+        self.secs = secs
+        
   
     def __str__(self):
-        return     
+        return  Calendar.__str__(self) + ", " +  Clock.__str__(self)  
+print(CalendarClock(20,10,2020,10,30,5))
