e07faea5389aec9d353928fa8d656b8e8014196e
FiodorG <gre_dorelus@yahoo.com>
2020-08-06 23:12:25 -0400

    solution to roman to integers

--- introduction_to_python/classes/2_roman_to_int/solution/solution.py
+++ introduction_to_python/classes/2_roman_to_int/solution/solution.py
@@ -1,4 +1,19 @@
 class py_solution:
     def roman_to_int(self, s):
-        rom_val {"I":1, "V"  }
-        return
+        rom_val = { "I" : 1, 
+                    "V" : 5,
+                    "X" : 10,
+                    "L" : 50,
+                    "C" : 100,
+                    "D" : 500,
+                    "M" : 1000
+        }
+        ints = 0
+        for i in range(len(s)):
+            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+                ints += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+            else:
+                ints += rom_val[s[i]]
+        return ints 
+
+                
