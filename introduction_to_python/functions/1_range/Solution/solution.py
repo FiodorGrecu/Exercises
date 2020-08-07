8a5adc1ef5a85ea8f9ae94ce17bd61b3b668cab5
FiodorG <gre_dorelus@yahoo.com>
2020-08-07 12:32:29 -0400

    checked something else

--- introduction_to_python/functions/1_range/Solution/solution.py
+++ introduction_to_python/functions/1_range/Solution/solution.py
@@ -2,7 +2,10 @@
 # range_100 = input("Enter a number: ")
 
 def range_100(number):
+    # range_100 = input("Enter a number: ")
+
     if number <= 100:
         return "GREATNESS"
     else:
         return "OOPS"   
+print(range_100(4))
