5c4bfc9aa329db20147daae88ff5658a82e52532
FiodorG <gre_dorelus@yahoo.com>
2020-08-10 16:11:26 -0400

    Array solution insert elem

--- data_structures/dictionaries_and_arrays/1_insert_element/solution/solution.py
+++ data_structures/dictionaries_and_arrays/1_insert_element/solution/solution.py
@@ -1,1 +1,7 @@
-# Write your solution here
+# Write your solution heref
+from array import array
+def insert_element(array_num,ins_pos,ins_val):
+    array_num.insert(ins_pos,ins_val)
+    return array_num
+
+print(insert_element(array("i",[1,2,3]),1,0))
