cb80310c37fc96b2e8520c764bf4568fadf7a23e
FiodorG <gre_dorelus@yahoo.com>
2020-08-11 10:44:25 -0400

    solution convert jason and update dictionary

--- data_structures/dictionaries_and_arrays/1_update_dict/solution/solution.py
+++ data_structures/dictionaries_and_arrays/1_update_dict/solution/solution.py
@@ -2,6 +2,8 @@
 def update_dict(key, value):
     new_dict = {}
     new_dict[key] = value
+    # new_dict.key = "value1"
     return new_dict
 
-
+# print(update_dict(new_dict))
+# print(new_dict)
--- data_structures/dictionaries_and_arrays/2_convert_json/solution/solution.py
+++ data_structures/dictionaries_and_arrays/2_convert_json/solution/solution.py
@@ -1,1 +1,5 @@
 # Write your solution here
+import json
+def convert(dict):
+    dictionary = json.dumps(dict)
+    return dictionary
