7d2197ff7540978a13fa4a910030d0f14a21d5f9
FiodorG <gre_dorelus@yahoo.com>
2020-08-13 17:36:49 -0400

    height of tree solution

--- data_structures/binary_trees/1_height_of_tree/solution/solution.py
+++ data_structures/binary_trees/1_height_of_tree/solution/solution.py
@@ -1,1 +1,26 @@
 # Write your solutions here
+class Node: 
+	def __init__(self,key,left=None,right=None): 
+		self.left = left
+		self.right = right
+		self.key = key
+    
+def height(node):
+    if node is None:
+        return 0
+    else:
+        lheight = height(node.left)
+        rheight = height(node.right)
+        if lheight > rheight:
+            return lheight + 1
+        else:
+            return rheight + 1
+
+root = Node(1) 
+root.left = Node(2) 
+root.right = Node(3) 
+root.left.left = Node(4) 
+root.left.right = Node(5) 
+  
+  
+print (f"Height of tree is:{height(root)}") 
