7d7b3551b65f892b906fe582a703c354e1bd3b9e
FiodorG <gre_dorelus@yahoo.com>
2020-08-13 16:20:34 -0400

    binary tree

--- data_structures/binary_trees/1_binary_tree/solution/solution.py
+++ data_structures/binary_trees/1_binary_tree/solution/solution.py
@@ -1,6 +1,19 @@
 #Write your solutions here
+class Node:
+    def __init__(self, key, left=None, right=None):
+        self.key = key
+        self.left = left
+        self.right = right
 
- 
+leftN = Node(4)
+rightN = Node(7)
+
+sample_tree = Node(5, leftN, rightN)
+
+print(sample_tree.key)
+print(sample_tree.left.key)
+print(sample_tree.right.key)
+
 
 
 
