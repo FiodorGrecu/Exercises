dfb8012163148b7c47cad07f1aa3e925849cb6b7
FiodorG <gre_dorelus@yahoo.com>
2020-08-13 12:15:39 -0400

    reverse_array solution

--- data_structures/dictionaries_and_arrays/2_reverse_array/solution/solution.py
+++ data_structures/dictionaries_and_arrays/2_reverse_array/solution/solution.py
@@ -1,1 +1,11 @@
 # Write your solution here
+from array import array
+def reverse(array_num):
+    reverse_array = array('i')
+    for i in range(len(array_num)-1,-1,-1):
+        reverse_array.append(array_num[i])
+    return reverse_array
+
+
+array_num = array("i", [1, 3, 5, 3, 7])
+print(reverse(array_num))
