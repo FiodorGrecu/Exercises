b7ca75539d7c701a4fdbef5b01223b1dd1bdf973
FiodorG <gre_dorelus@yahoo.com>
2020-08-14 12:01:55 -0400

    solution of insert val and nth order

--- data_structures/binary_trees/3_closest_value/solution/solution.py
+++ data_structures/binary_trees/3_closest_value/solution/solution.py
@@ -5,9 +5,30 @@ class Node:
         self.right = right
         self.key = key
 
+def closest_value_node(node, target, prev):
+    if node:
+        if node.key == target:
+            return node.key
+        child = node.left if node.key > target else node.right
+        closest = closest_value_node(child, target, node.key)
+        if abs(closest - target) < abs(node.key - target):
+            return closest
+        else:
+            return node.key
+    return prev
+
 
 def closest_value(node, target):
-    return
+    return closest_value_node(node, target, node.key)
 
 
 
+root = Node(6)
+root.left = Node(4)
+root.right = Node(10)
+root.left.left = Node(3)
+root.left.right = Node(5)
+root.right.left = Node(8)
+root.right.right = Node(12)
+
+print(closest_value(root, 12))
--- data_structures/binary_trees/3_nth_inorder/solution/solution.py
+++ data_structures/binary_trees/3_nth_inorder/solution/solution.py
@@ -1,1 +1,31 @@
 #Write your solutions here
+class Node: 
+	def __init__(self,key,left=None,right=None): 
+		self.left = left
+		self.right = right
+		self.key = key
+
+def Inorder(root):
+    if not root:
+        return []
+    return Inorder(root.left) + [root.key] + Inorder(root.right)
+
+
+def NthInorder(root, n):         
+    inorder_keys = Inorder(root)
+    if len(inorder_keys) > n:
+
+        return inorder_keys[n-1]
+    else:
+        return f'no {n}-th element'
+
+items = Node(5)
+items.left = Node(6)
+items.right = Node(7)
+items.left.left = Node(8)
+items.left.right = Node(9)
+ 
+
+
+print(Inorder(items))
+print(NthInorder(items,3))
