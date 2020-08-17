e70dc71350e7013cbdcd7c4996c214191181dba1
FiodorG <gre_dorelus@yahoo.com>
2020-08-17 12:17:50 -0400

    get prrent solution

--- data_structures/heaps/1_get_parent_node/solution/solution.py
+++ data_structures/heaps/1_get_parent_node/solution/solution.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def get_parent_node(heap, index):
+    parent_idx = (index-1)//2
+    if index == 0 and len(heap) > 0:
+        return heap[0]
+    if index < len(heap):
+        return heap[parent_idx]
+    else:
+        return f'{index} is not a valid index in the heap'
+        
+print(get_parent_node([2,3,4,5,10], 4))
+print(get_parent_node([2,3,4,5,10], 0))
+print(get_parent_node([2,3,4,5,10], 5))
