e3331c70703a3363b3df8b24b6a01323fd41d7c4
FiodorG <gre_dorelus@yahoo.com>
2020-08-17 11:41:20 -0400

    solution to get child

--- data_structures/heaps/1_get_child_nodes/solution/solution.py
+++ data_structures/heaps/1_get_child_nodes/solution/solution.py
@@ -1,5 +1,15 @@
 # Write your solution here
+def get_child_nodes(heap, index):
+    left_index = 2*index + 1
+    right_index = 2* index + 2 
+    chil_lst = []
+    if left_index < len(heap):
+        chil_lst.append(heap[left_index])
+    if right_index < len(heap):
+        chil_lst.append(heap[right_index])
+    return chil_lst
 
-
+print(get_child_nodes([2,3,4,5,10], 0))
+print(get_child_nodes([2, 3, 4, 5, 10], 1))
 
     
