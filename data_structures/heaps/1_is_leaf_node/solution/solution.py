23ed63bdeae4c7026adefaa5eb03301e084d5012
FiodorG <gre_dorelus@yahoo.com>
2020-08-17 12:33:02 -0400

    solution to leaf

--- data_structures/heaps/1_is_leaf_node/solution/solution.py
+++ data_structures/heaps/1_is_leaf_node/solution/solution.py
@@ -1,6 +1,8 @@
 # Write your solution here
 def is_leaf(heap, index): 
-        return
+        if index >= (len(heap)// 2) and index < len(heap):
+                return True
+        return False
 
 
-    
+print(is_leaf([1, 2, 3, 4, 5, 6], 1))
