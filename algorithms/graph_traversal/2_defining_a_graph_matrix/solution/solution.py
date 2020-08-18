b1b19443ded88e9197151fadaf6cc5c266af2735
FiodorG <gre_dorelus@yahoo.com>
2020-08-17 22:02:57 -0400

    solution to matrix graph

--- algorithms/graph_traversal/2_defining_a_graph_matrix/solution/solution.py
+++ algorithms/graph_traversal/2_defining_a_graph_matrix/solution/solution.py
@@ -1,1 +1,15 @@
-adjacency_matrix = []
+adjacency_matrix = [[0,1,0],
+                    [1,0,1],
+                    [0,1,0]]
+
+# 1->1 = 0 (from first node to first node there is no edge)
+# 1->2 = 1 (first edge )
+# 2->2 = 0 (from second node to second node there is no edge)
+
+# 2->1 = 1
+# 1->1 = 0
+# 1->3 = 1
+
+# 3->3 = 0
+# 3->1 = 1
+# 1->1 = 0
