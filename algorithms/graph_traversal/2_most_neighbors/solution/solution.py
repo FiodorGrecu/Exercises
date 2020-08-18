44d63c2c63a3bbc7528e9f0ef6aebee26e87a4b1
FiodorG <gre_dorelus@yahoo.com>
2020-08-17 22:28:03 -0400

    most neightbour solution

--- algorithms/graph_traversal/2_most_neighbors/solution/solution.py
+++ algorithms/graph_traversal/2_most_neighbors/solution/solution.py
@@ -1,2 +1,11 @@
 def most_neighbours(adjacency_list):
-    return
+    max_len = -1
+    max_key = -1
+    for keyNode, edge_lst in adjacency_list.items():
+        num_neighbors = len(edge_lst)
+        if num_neighbors > max_len:
+            max_key = keyNode
+            max_len = num_neighbors
+        elif num_neighbors == max_len and keyNode < max_key:
+            max_key = keyNode
+    return max_key
