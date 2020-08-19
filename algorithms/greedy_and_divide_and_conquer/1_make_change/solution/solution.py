0d724202624bcf07d083e4dc0b39952ad63bad51
FiodorG <gre_dorelus@yahoo.com>
2020-08-19 12:37:32 -0400

    solution make change and make change lmited

--- algorithms/greedy_and_divide_and_conquer/1_make_change/solution/solution.py
+++ algorithms/greedy_and_divide_and_conquer/1_make_change/solution/solution.py
@@ -1,3 +1,14 @@
 
+def make_change_helper(amount, denominations, number_coins):
+    for i in range(len(denominations)-1,-1,-1):
+        if amount >= denominations[i]:
+            if number_coins == -1:
+                number_coins +=1
+            return make_change_helper(amount - denominations[i], denominations, number_coins+1)
+    return number_coins
+
 def make_change(amount, denominations):
-    return
+    return make_change_helper(amount, denominations, -1)
+
+denominations = [1, 2, 5, 10, 20, 50, 100, 200]
+print(make_change(371, denominations))
--- algorithms/greedy_and_divide_and_conquer/2_make_change_limited_coins/solution/solution.py
+++ algorithms/greedy_and_divide_and_conquer/2_make_change_limited_coins/solution/solution.py
@@ -1,2 +1,24 @@
+def make_change_helper(amount,denominations, denominations_keys, num_coins):
+    if amount == 0:
+        return num_coins
+    for index, k in enumerate(denominations_keys):
+        if amount >= k:
+            denominations[k] -= 1
+            if denominations[k] == 0:
+                denominations_keys = denominations_keys[index+1:]
+            return make_change_helper(amount-k, denominations, denominations_keys, num_coins+1)
+    return -1
+
+
 def make_change(amount, denominations):
-    return
+    denominations_keys = sorted(denominations.keys(), reverse=True)
+    return make_change_helper(amount, denominations, denominations_keys, 0)
+
+denominations = {1: 5, 2: 1, 10: 5, 50: 3, 100: 1, 200: 1}
+print(make_change(450, denominations))
+
+denominations2 = {1: 5, 2: 1, 5: 1, 10: 1, 20: 1, 50: 3}
+print(make_change(43, denominations2))
+
+denominations3 = {1: 5, 2: 1, 50: 3, 100: 1, 200: 1}
+print(make_change(305, denominations3))
