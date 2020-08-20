a699f3a1c03bff355faba125689b0580fd2057ed
FiodorG <gre_dorelus@yahoo.com>
2020-08-20 12:22:20 -0400

    median of medians solution and frequency arraycumulative

--- algorithms/sorting/2_frequency_array_cumulative/solution/solution.py
+++ algorithms/sorting/2_frequency_array_cumulative/solution/solution.py
@@ -1,3 +1,27 @@
+def frequency_array(lst):
+    max_elem = -1
+    for i in lst:
+        if i > max_elem:
+            max_elem = i
+    frequency_list = [0 for i in range(max_elem+1)] 
+
+    for i in range(len(lst)):
+        frequency_list[lst[i]] += 1
+    return frequency_list
+
 def frequency_array_cumulative(lst):
-    return
+    freq_lst = frequency_array(lst)
+    freq_len = len(freq_lst)
+    freq_cum_lst = [0 for i in range(freq_len)]
+    for i in range(freq_len):
+        if i == 0:
+            freq_cum_lst[i] = freq_lst[i]
+        else:
+            freq_cum_lst[i] = freq_lst[i] + freq_cum_lst[i-1]
+    return freq_cum_lst
+
+
+# lst = [1, 1, 4, 3, 8, 5, 9, 8, 8, 4, 6]
+
+print(frequency_array_cumulative([1, 1, 4, 3, 8, 5, 9, 8, 8, 4, 6]))
 
--- algorithms/sorting/2_median_of_medians/solution/solution.py
+++ algorithms/sorting/2_median_of_medians/solution/solution.py
@@ -1,2 +1,15 @@
 def median_of_medians(lst):
-    return
+    sublists = [lst[j:j+5] for j in range(0, len(lst),5)]
+    # print(sublists)
+
+    medians = []
+
+    for sublist in sublists:
+        medians.append(sorted(sublist)[len(sublist)//2])
+        # print(medians)
+    if len(medians) <= 5:
+        return sorted(medians)[len(medians)//2]
+    else:
+        return median_of_medians(medians)
+
+print(median_of_medians([9, 1, 0, 2, 3, 4, 6, 8, 7, 12, 5]))
