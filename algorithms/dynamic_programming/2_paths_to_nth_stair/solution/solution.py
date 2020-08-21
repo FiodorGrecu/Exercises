f88c224d94e3f33d04767b2ad2ec39ce32356f6b
FiodorG <gre_dorelus@yahoo.com>
2020-08-21 14:44:22 -0400

    solution to Path to nstairs and Shortest path

--- algorithms/dynamic_programming/2_paths_to_nth_stair/solution/solution.py
+++ algorithms/dynamic_programming/2_paths_to_nth_stair/solution/solution.py
@@ -1,5 +1,27 @@
 def paths_nth_stair(n):
-    return
+    if n < 2:
+        return n
+    else:
+        memo = [-1 for i in range(n+1)]
+        memo[1] = 1
+        memo[2] = 2 
+        for i in range(3, n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+        return memo[n]
+
+print(paths_nth_stair(4))
+
+# def paths_nth_stair(n):
+#     if n <= 1: 
+#         return n 
+#     else:
+#         return paths_nth_stair(n-1) + paths_nth_stair(n-2)
+
+# def count_ways(steps): 
+#     return paths_nth_stair(steps + 1)
+
+# # print(count_ways(10))
 
 
 
+# print(paths_nth_stair(3))
--- algorithms/dynamic_programming/2_shortest_path_to_1/solution/solution.py
+++ algorithms/dynamic_programming/2_shortest_path_to_1/solution/solution.py
@@ -1,2 +1,38 @@
 def shortest_path_to_1(n):
-    return
+    if n == 1:
+        return 0
+    if  n == 2 or n == 3:
+        return 1
+    memo = [-1 for i in range(n+1)]
+    memo[1] = 0
+    memo[2] = 1
+    memo[3] = 1
+    for i in range(4, n+1):
+        if memo[i] == -1:
+            if i%3 == 0 and 1%2 ==0:
+                memo[i] = 1 + min(memo[i-1], memo[i//3], memo[i//2])
+            elif i%3 == 0:
+                memo[i] = 1 + min(memo[i-1], memo[i//3])
+            elif i%2 == 0:
+                memo[i] = 1 + min(memo[i-1], memo[i//2])
+            else:
+                memo[i] =1 + memo[i-1]
+    return memo[n]
+
+print(shortest_path_to_1(10))
+
+        
+            
+# def shortest_path_to_1(n):
+# 	count = 0
+# 	while n > 1:
+# 		if n % 2 == 0:             
+# 			n = n // 2
+# 		elif n == 3 or n % 4 == 1: 
+# 			n = n - 1
+# 		else:
+# 			n = n + 1              
+# 		count += 1
+# 	return count
+# print(shortest_path_to_1(1))
+
