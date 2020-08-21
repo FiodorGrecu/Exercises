6af264d7cf5cdab88dd9d8ab0a4111452199608b
FiodorG <gre_dorelus@yahoo.com>
2020-08-21 17:02:44 -0400

    tribonaci solution

--- algorithms/dynamic_programming/2_Tribonacci/solution/solution.py
+++ algorithms/dynamic_programming/2_Tribonacci/solution/solution.py
@@ -1,2 +1,16 @@
+# def tribonacci(n):
+
 def tribonacci(n):
-    return
+    if n == 0 or n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    memo = [-1 for i in range(n+1)]
+    memo[0] = 0
+    memo[1] = 0
+    memo[2] = 1
+    for i in range(3, n+1):
+        memo[i] = memo[i-3] + memo[i-2] + memo[i-1]
+    return memo[i]
+
+print(tribonacci(10))
