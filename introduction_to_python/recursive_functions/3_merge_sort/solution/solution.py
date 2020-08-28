0ec9250de7d3b2cc15d21bccf8533d72cfbb2328
FiodorG <gre_dorelus@yahoo.com>
2020-08-27 22:17:45 -0400

    merge sort solution

--- introduction_to_python/recursive_functions/3_merge_sort/solution/solution.py
+++ introduction_to_python/recursive_functions/3_merge_sort/solution/solution.py
@@ -1,3 +1,28 @@
 # Write your solution here
 def custom_sort(num_list):
-    return
+    if len(num_list)>1: 
+        m = len(num_list)//2
+        left = num_list[:m] 
+        right = num_list[m:] 
+        left = custom_sort(left) 
+        right = custom_sort(right) 
+  
+        num_list =[] 
+  
+        while len(left)>0 and len(right)>0: 
+            if left[0]<right[0]: 
+                num_list.append(left[0]) 
+                left.pop(0) 
+            else: 
+                num_list.append(right[0]) 
+                right.pop(0) 
+  
+        for i in left: 
+            num_list.append(i) 
+        for i in right: 
+            num_list.append(i) 
+                  
+    return num_list 
+alist = [12, 11, 13, 5, 6, 7]
+alist = custom_sort(alist)
+print(alist)
