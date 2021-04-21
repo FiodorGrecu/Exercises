a3d711b68b46df8295c7e5efacc8579f6639039d
FiodorG <gre_dorelus@yahoo.com>
2021-04-21 13:56:52 -0400

    Byte exercises retry

--- introduction_and_environment/data_types_and_control_flow/1_string_length/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/1_string_length/Solution/solution.py
@@ -1,5 +1,6 @@
 # Code your solution here
-string_1 = input("Enter a string: ")
+string_1 = "Hello"
+# string_1 = input("Enter a string: ")
 string_value = len(string_1)
 data = string_value
 print(data)
